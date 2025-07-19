15e872c9bc304f4909b82ba6c5beb53edd3f7bab
OneDev49 <dev49.sgra@protonmail.com>
2025-07-19 23:02:50 +0200

    feat(icon): create status icon components

---
+++ src/components/icons/status/CheckIcon.tsx
@@ -0,0 +1,49 @@
+/**
+ * @license
+ * SVG Code by:
+ * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license/free
+ * Copyright 2025 Fonticons, Inc.
+ *
+ * For more information, check the LICENSE.txt of the Repository
+ */
+
+const iconVariants = {
+  solid: {
+    viewBox: '0 0 448 512',
+    path: 'M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z',
+  },
+};
+
+type IconVariant = keyof typeof iconVariants;
+
+interface IconProps {
+  width?: number | string;
+  height?: number | string;
+  className?: string;
+  color?: string;
+  variant?: IconVariant;
+}
+
+export default function CheckIcon({
+  width = 24,
+  height = 24,
+  className,
+  variant = 'solid',
+  color,
+}: IconProps) {
+  const selectedVariant = iconVariants[variant];
+
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox={selectedVariant.viewBox}
+      width={width}
+      height={height}
+      fill={color || 'currentColor'}
+      className={className}
+    >
+      <path d={selectedVariant.path} />
+    </svg>
+  );
+}
---
+++ src/components/icons/status/InfoIcon.tsx
@@ -0,0 +1,49 @@
+/**
+ * @license
+ * SVG Code by:
+ * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license/free
+ * Copyright 2025 Fonticons, Inc.
+ *
+ * For more information, check the LICENSE.txt of the Repository
+ */
+
+const iconVariants = {
+  solid: {
+    viewBox: '0 0 512 512',
+    path: 'M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336l24 0 0-64-24 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l48 0c13.3 0 24 10.7 24 24l0 88 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-80 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z',
+  },
+};
+
+type IconVariant = keyof typeof iconVariants;
+
+interface IconProps {
+  width?: number | string;
+  height?: number | string;
+  className?: string;
+  color?: string;
+  variant?: IconVariant;
+}
+
+export default function InfoIcon({
+  width = 24,
+  height = 24,
+  className,
+  variant = 'solid',
+  color,
+}: IconProps) {
+  const selectedVariant = iconVariants[variant];
+
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox={selectedVariant.viewBox}
+      width={width}
+      height={height}
+      fill={color || 'currentColor'}
+      className={className}
+    >
+      <path d={selectedVariant.path} />
+    </svg>
+  );
+}
