535639c3499876312f0582c3e1456684910aba3e
OneDev49 <dev49.sgra@protonmail.com>
2025-07-21 13:40:58 +0200

    styles: add tailwind styles to button variants

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import clsx from 'clsx';
 
 interface ButtonBaseProps {
-  variant?: 'primary' | 'secondary' | 'terciary';
+  variant?: 'primary' | 'secondary' | 'tertiary';
   children: React.ReactNode;
   className?: string;
 }
@@ -21,8 +21,31 @@ export default function Button<C extends React.ElementType = typeof Link>({
 }: ButtonProps<C>) {
   const Component = as || Link;
 
+  const buttonClassNames = clsx(
+    'p-[12px_18px]',
+    'rounded-2xl',
+    'text-xl',
+    'transition-all',
+    'hover:scale-[1.02]',
+    'flex',
+    'flex-row',
+    'gap-2',
+    'items-center',
+    {
+      [`bg-[linear-gradient(90deg,#fe9043,#ff6c06)] text-black hover:bg-[linear-gradient(90deg,#ffa769,#ff7a1d)]`]:
+        variant == 'primary',
+      [`bg-[linear-gradient(90deg,#06480a,#0e8e17)] shadow-[0_4px_4px_0_rgba(0,255,68,0.4)] hover:bg-[linear-gradient(90deg,#075d0c,#0ca816)]`]:
+        variant == 'secondary',
+      [`bg-[rgba(0,0,0,0.4)] border border-white shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] hover:bg-[rgba(0,0,0,1)]`]:
+        variant == 'tertiary',
+    }
+  );
+
   return (
-    <Component className={className} {...(restProps as any)}>
+    <Component
+      className={`${buttonClassNames} ${className || ''}`}
+      {...(restProps as any)}
+    >
       {children}
     </Component>
   );
