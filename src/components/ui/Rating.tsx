87c21a43f4449630adea4df676a49581fb51cdb4
OneDev49 <dev49.sgra@protonmail.com>
2025-07-21 21:34:54 +0200

    feat(ui): create Rating ui component

---
+++ src/components/ui/Rating.tsx
@@ -0,0 +1,63 @@
+import clsx from 'clsx';
+import StarIcon from '../icons/ecommerce/StarIcon';
+import styles from './Rating.module.scss';
+
+interface RatingProps {
+  rating: number;
+  maxRating?: number;
+  className?: string;
+}
+
+export default function Rating({
+  rating,
+  maxRating = 5,
+  className,
+}: RatingProps) {
+  const percentage = Math.max(0, Math.min((rating / maxRating) * 100, 100));
+
+  const ratingContainerClassNames = clsx(
+    'relative',
+    'inline-flex',
+    'items-center'
+  );
+
+  const starsForegroundClassNames = clsx(
+    'absolute',
+    'top-[0]',
+    'left-[0]',
+    'flex',
+    'whitespace-nowrap',
+    'overflow-hidden'
+  );
+
+  const starsClassNames = clsx('w-4', 'h-4', 'shrink-0');
+
+  const starsFilledClassNames = clsx('text-[#1eff00]');
+
+  const starsEmptyClassNames = clsx('text-[#4a5568]');
+
+  return (
+    <div className={`${ratingContainerClassNames} ${className}`}>
+      <div className='flex'>
+        {Array.from({ length: maxRating }).map((_, index) => (
+          <StarIcon
+            key={index}
+            className={`${starsClassNames} ${starsEmptyClassNames}`}
+          />
+        ))}
+      </div>
+
+      <div
+        className={starsForegroundClassNames}
+        style={{ width: `${percentage}%` }}
+      >
+        {Array.from({ length: maxRating }).map((_, index) => (
+          <StarIcon
+            key={index}
+            className={`${starsClassNames} ${starsFilledClassNames}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
