bcba560fa5329349fb95e972f8d7775f6de56ae3
OneDev49 <dev49.sgra@protonmail.com>
2025-08-01 03:11:46 +0200

    feat(db): create prisma.ts for database fetching

---
+++ src/lib/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+const prismaClientSingelton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingelton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingelton();
+
+export { prisma };
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = prisma;
+}
