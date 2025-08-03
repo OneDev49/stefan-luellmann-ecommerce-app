d76939b0f2c2c42c988b28332ac9fa92016cd7e7
OneDev49 <dev49.sgra@protonmail.com>
2025-08-03 23:57:07 +0200

    feat: add technical Data to DescriptionSection of product page

--- src/app/(main)/(shop)/product/[slug]/_components/DescriptionSection.tsx
+++ src/app/(main)/(shop)/product/[slug]/_components/DescriptionSection.tsx
@@ -11,14 +11,41 @@ export default function DescriptionSection({
     'bg-[rgb(33,33,33,0.5)] border border-[#6c6c6c] rounded-3xl max-w-7xl m-auto'
   );
 
+  const headingClassNames = clsx('text-3xl font-bold p-8');
+
+  const headingsBGClassNames = clsx('p-8');
+
+  const specs = productItem?.specs || {};
+
   return (
     <section
-      className={`${transparentCardClassName} flex flex-col justify-between overflow-hidden`}
+      className={`${transparentCardClassName} flex justify-between overflow-hidden relative`}
     >
-      <h2 className='p-8 bg-[rgb(87,87,87,0.2)] shadow-[0_4px_15px_0_rgb(0,0,0,1)] text-3xl font-bold'>
-        Product Description
-      </h2>
-      <div className='p-8'>{productItem.longDescription}</div>
+      <div className='absolute h-24 bg-[rgb(87,87,87,0.2)] shadow-[0_4px_15px_0_rgb(0,0,0,1)] left-0 right-0'></div>
+      <div className='flex-1 relative'>
+        <h2 className={headingClassNames}>Technical Data</h2>
+        <table className='table-auto border-collapse text-sm w-full my-8 mx-4 max-w-[90%]'>
+          <tbody>
+            {Object.entries(specs).map(([key, value]) => (
+              <tr
+                key={key}
+                className='border-b border-gray-700 text-base hover:bg-[#007623] cursor-pointer'
+              >
+                <td className='px-4 py-2 font-medium text-left capitalize'>
+                  {key
+                    .replace(/([a-z])([A-Z])/g, '$1 $2')
+                    .replace(/^./, (c) => c.toUpperCase())}
+                </td>
+                <td className='px-4 py-2 text-left'>{String(value)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className='flex-1 relative'>
+        <h2 className={headingClassNames}>Product Description</h2>
+        <div className='p-8 leading-7'>{productItem.longDescription}</div>
+      </div>
     </section>
   );
 }
