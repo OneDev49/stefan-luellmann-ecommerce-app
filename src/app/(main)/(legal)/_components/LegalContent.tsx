3cfc7711f03016409eecebc7089f2df74fb29605
OneDev49 <dev49.sgra@protonmail.com>
2025-08-23 19:25:05 +0200

    feat(legal): create legal pages and legal page component

---
+++ src/app/(main)/(legal)/_components/LegalContent.tsx
@@ -0,0 +1,15 @@
+interface LegalContentProps {
+  heading: string;
+  children: React.ReactNode;
+}
+
+export default function LegalContent({ heading, children }: LegalContentProps) {
+  return (
+    <section className='max-w-7xl mx-auto flex py-8 min-h-screen'>
+      <div className='basis-[65ch] space-y-8'>
+        <h1 className='text-5xl'>{heading}</h1>
+        {children}
+      </div>
+    </section>
+  );
+}
