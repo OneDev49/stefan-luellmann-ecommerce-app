727d286bc0e3933c2ced1961d707711efa91b464
OneDev49 <dev49.sgra@protonmail.com>
2025-09-08 15:15:15 +0200

    refactor(header): separate RightSidenav into smaller components

--- src/components/layout/_components/RightSidenav.tsx
+++ src/components/layout/_components/RightSidenav.tsx
@@ -5,19 +5,8 @@ import CloseIcon from '@/components/icons/ui/CloseIcon';
 import CartIcon from '@/components/icons/ecommerce/CartIcon';
 import HeartIcon from '@/components/icons/ecommerce/HeartIcon';
 import clsx from 'clsx';
-import {
-  useCartStore,
-  selectTotalItems,
-  selectCartTotal,
-} from '@/store/cartStore';
-import Image from 'next/image';
-import Link from 'next/link';
-import {
-  selectWishlistTotalItems,
-  useWishlistStore,
-} from '@/store/wishlistStore';
-import Button from '@/components/ui/Button';
-import TrashIcon from '@/components/icons/ecommerce/TrashIcon';
+import HeaderCart from './tabs/CartTab';
+import HeaderWishlist from './tabs/WishlistTab';
 
 interface RightSidenavProps {
   onClose: () => void;
@@ -33,21 +22,6 @@ export default function RightSidenav({
   /* Active Tab Management */
   const [currentActiveTab, setCurrentActiveTab] = useState(activeTab);
 
-  /* Zustand Cart Store */
-  const cartItems = useCartStore((state) => state.items);
-  const removeFromCart = useCartStore((state) => state.removeFromCart);
-  const clearCart = useCartStore((state) => state.clearCart);
-  const totalCartValue = useCartStore(selectCartTotal);
-  const totalCartAmount = useCartStore(selectTotalItems);
-
-  /* Zustand Wishlist Store */
-  const wishlistItems = useWishlistStore((state) => state.items);
-  const removeFromWishlist = useWishlistStore(
-    (state) => state.removeFromWishlist
-  );
-  const clearWishlist = useWishlistStore((state) => state.clearWishlist);
-  const totalWishlistAmount = useWishlistStore(selectWishlistTotalItems);
-
   /* Tab CSS Tailwind Classes */
   const tabClassNames = clsx(
     'flex gap-2 bg-[#075d0c] py-1 px-3 rounded-3xl transition-colors border border-[#0ca816] hover:bg-[#1f702a]'
@@ -55,20 +29,6 @@ export default function RightSidenav({
   const tabAnnouncerClassNames = clsx(
     'p-2 bg-[#2d2d2d] border border-gray-500 shadow-[0_4px_15px_0_rgb(140,140,140,0.25)] rounded-md text-center'
   );
-
-  /* Handle RemovefromCart */
-  const handleRemoveFromCart = (e: React.MouseEvent, productId: string) => {
-    e.stopPropagation();
-    e.preventDefault();
-    removeFromCart(productId);
-  };
-
-  /* Handle RemoveFromWishlist */
-  const handleRemoveFromWishlist = (e: React.MouseEvent, productId: string) => {
-    e.stopPropagation();
-    e.preventDefault();
-    removeFromWishlist(productId);
-  };
 
   return (
     <div
@@ -125,241 +85,13 @@ export default function RightSidenav({
 
         <div className='relative flex-1 px-4 py-6 overflow-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-green-500'>
           {currentActiveTab === 'cart' && (
-            <>
-              {cartItems.length > 0 ? (
-                <div className='space-y-5'>
-                  <div className='border-b border-white pb-4 space-y-4'>
-                    <div className='space-y-2'>
-                      <strong className='text-xl flex justify-between items-center'>
-                        <span>Quantity of your Cart: </span>
-                        <span className='underline text-2xl'>
-                          {totalCartAmount}
-                        </span>
-                      </strong>
-                      <strong className='text-xl flex justify-between items-center'>
-                        <span>Your Cart Value: </span>
-                        <span className='underline text-2xl'>
-                          {totalCartValue.toFixed(2)}€
-                        </span>
-                      </strong>
-                    </div>
-                    <div className='flex justify-between'>
-                      <Button
-                        href='/checkout'
-                        onClick={onClose}
-                        variant='primary'
-                        position='card'
-                      >
-                        <CartIcon height={20} width={20} />
-                        To Checkout
-                      </Button>
-                      <Button
-                        as='button'
-                        type='button'
-                        onClick={clearCart}
-                        variant='danger'
-                        position='card'
-                      >
-                        <TrashIcon height={20} width={20} />
-                        Clear Cart
-                      </Button>
-                    </div>
-                  </div>
-                  <ul className='list-none m-0 p-0 space-y-3'>
-                    {cartItems.map((item) => (
-                      <li key={item.id}>
-                        <Link
-                          href={`/product/${item.slug}`}
-                          className='p-2 border border-[#1e9c29] rounded-lg flex gap-2 justify-between bg-[#103d22] hover:bg-[#134929] transition-colors'
-                          onClick={onClose}
-                        >
-                          <div className='flex gap-2'>
-                            <div className='bg-white min-w-24 h-24 w-24 block rounded-lg overflow-hidden relative'>
-                              {item.isOnSale && (
-                                <div className='absolute text-sm bg-red-700 h-8 grid place-items-center w-32 top-[8px] right-[-40px] rotate-45 z-50 will-change-transform'>
-                                  On Sale
-                                </div>
-                              )}
-                              <Image
-                                className='object-contain w-full'
-                                src={`https://utfs.io/a/5sfnefg5kv/${item.imageUrl}`}
-                                height={90}
-                                width={90}
-                                alt={item.name}
-                                draggable='false'
-                                decoding='async'
-                                loading='lazy'
-                              />
-                            </div>
-                            <div className='flex flex-col gap-1'>
-                              <strong className='line-clamp-1 text-lg'>
-                                {item.name}
-                              </strong>
-                              <div className='flex items-center gap-2'>
-                                <span className='text-sm'>In Your Cart:</span>
-                                <span>{item.quantity}</span>
-                              </div>
-                              <div className='flex items-center gap-2'>
-                                {item.isOnSale && item.reducedPrice ? (
-                                  <>
-                                    <span className='text-sm'>Price:</span>
-                                    <span className='text-red-500 font-bold'>
-                                      {item.reducedPrice.toFixed(2)}€
-                                    </span>
-                                    <span className='text-xs text-red-500 line-through'>
-                                      {item.price.toFixed(2)}€
-                                    </span>
-                                  </>
-                                ) : (
-                                  <>
-                                    <span className='text-sm'>Price:</span>
-                                    <span>{item.price.toFixed(2)}€</span>
-                                  </>
-                                )}
-                              </div>
-                            </div>
-                          </div>
-                          <div className='min-w-4'>
-                            <Button
-                              as='button'
-                              variant='danger'
-                              position='standalone'
-                              onClick={(e) => handleRemoveFromCart(e, item.id)}
-                              className='rounded-sm'
-                            >
-                              <CloseIcon height={20} width={20} />
-                            </Button>
-                          </div>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : (
-                <p className={tabAnnouncerClassNames}>
-                  Your shopping cart is empty.
-                </p>
-              )}
-            </>
+            <HeaderCart onClose={onClose} className={tabAnnouncerClassNames} />
           )}
           {currentActiveTab === 'wishlist' && (
-            <>
-              {wishlistItems.length > 0 ? (
-                <div className='space-y-5'>
-                  <div className='border-b border-white pb-4 space-y-4'>
-                    <div className='ml-auto max-w-fit'>
-                      <Button
-                        as='button'
-                        type='button'
-                        variant='danger'
-                        position='card'
-                        onClick={clearWishlist}
-                      >
-                        <TrashIcon height={20} width={20} />
-                        Clear Wishlist
-                      </Button>
-                    </div>
-                  </div>
-                  <ul className='list-none m-0 p-0 space-y-3'>
-                    {wishlistItems.map((item) => (
-                      <li key={item.id}>
-                        <Link
-                          href={`/product/${item.slug}`}
-                          className='p-2 border border-[#1e9c29] rounded-lg flex gap-2 justify-between bg-[#103d22] hover:bg-[#134929] transition-colors'
-                          onClick={onClose}
-                        >
-                          <div className='flex gap-2'>
-                            <div className='bg-white min-w-24 h-24 w-24 block rounded-lg overflow-hidden relative'>
-                              {item.isOnSale && (
-                                <div className='absolute text-sm bg-red-700 h-8 grid place-items-center w-32 top-[8px] right-[-40px] rotate-45 z-50 will-change-transform'>
-                                  On Sale
-                                </div>
-                              )}
-                              <Image
-                                className='object-contain w-full'
-                                src={`https://utfs.io/a/5sfnefg5kv/${item.imageUrl}`}
-                                height={90}
-                                width={90}
-                                alt={item.name}
-                                draggable='false'
-                                decoding='async'
-                                loading='lazy'
-                              />
-                            </div>
-                            <div className='flex flex-col gap-1'>
-                              <strong className='line-clamp-1 text-lg'>
-                                {item.name}
-                              </strong>
-                              <div className='flex items-center gap-2'>
-                                {item.isOnSale && item.reducedPrice ? (
-                                  <>
-                                    <span className='text-sm'>Price:</span>
-                                    <span className='text-red-500 font-bold'>
-                                      {item.reducedPrice.toFixed(2)}€
-                                    </span>
-                                    <span className='text-xs text-red-500 line-through'>
-                                      {item.price.toFixed(2)}€
-                                    </span>
-                                  </>
-                                ) : (
-                                  <>
-                                    <span className='text-sm'>Price:</span>
-                                    <span>{item.price.toFixed(2)}€</span>
-                                  </>
-                                )}
-                              </div>
-                              <div className='text-sm flex gap-2'>
-                                <p
-                                  className={clsx({
-                                    ['text-[#00ff55]']: item.stockCount >= 100,
-                                    ['text-[#ffe100]']: item.stockCount < 100,
-                                    ['text-[#ff0000]']: item.stockCount === 0,
-                                  })}
-                                >
-                                  {item.stockCount === 0
-                                    ? `Out of Stock`
-                                    : item.stockCount < 10 &&
-                                      item.stockCount > 0
-                                    ? `Limited Stock`
-                                    : `In Stock`}
-                                </p>
-                                <p>
-                                  |{' '}
-                                  {item.stockCount > 10
-                                    ? `>10`
-                                    : `${item.stockCount}`}{' '}
-                                  in Storage
-                                </p>
-                              </div>
-                            </div>
-                          </div>
-                          <div className='min-w-4'>
-                            <Button
-                              as='button'
-                              type='button'
-                              variant='danger'
-                              position='standalone'
-                              onClick={(e) =>
-                                handleRemoveFromWishlist(e, item.id)
-                              }
-                              className='bg-red-500 rounded-sm'
-                            >
-                              <CloseIcon height={20} width={20} />
-                            </Button>
-                          </div>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : (
-                <div>
-                  <p className={tabAnnouncerClassNames}>
-                    Your wishlist is empty.
-                  </p>
-                </div>
-              )}
-            </>
+            <HeaderWishlist
+              onClose={onClose}
+              className={tabAnnouncerClassNames}
+            />
           )}
         </div>
       </div>
---
+++ src/components/layout/_components/tabs/CartTab.tsx
@@ -0,0 +1,146 @@
+import CartIcon from '@/components/icons/ecommerce/CartIcon';
+import TrashIcon from '@/components/icons/ecommerce/TrashIcon';
+import CloseIcon from '@/components/icons/ui/CloseIcon';
+import Button from '@/components/ui/Button';
+import {
+  selectCartTotal,
+  selectTotalItems,
+  useCartStore,
+} from '@/store/cartStore';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface HeaderCartProps {
+  onClose: () => void;
+  className: string;
+}
+
+export default function HeaderCart({ onClose, className }: HeaderCartProps) {
+  /* Zustand Cart Store */
+  const cartItems = useCartStore((state) => state.items);
+  const removeFromCart = useCartStore((state) => state.removeFromCart);
+  const clearCart = useCartStore((state) => state.clearCart);
+  const totalCartValue = useCartStore(selectCartTotal);
+  const totalCartAmount = useCartStore(selectTotalItems);
+
+  const handleRemoveFromCart = (e: React.MouseEvent, productId: string) => {
+    e.stopPropagation();
+    e.preventDefault();
+    removeFromCart(productId);
+  };
+
+  return (
+    <>
+      {cartItems.length > 0 ? (
+        <div className='space-y-5'>
+          <div className='border-b border-gray-400'>
+            <div className='space-y-1 border-b border-gray-400 pb-6'>
+              <strong className='text-lg flex justify-between items-center'>
+                <span>Products in Cart: </span>
+                <span className='underline text-lg'>{totalCartAmount}</span>
+              </strong>
+              <strong className='text-lg flex justify-between items-center'>
+                <span>Your Cart Value: </span>
+                <span className='underline text-lg'>
+                  {totalCartValue.toFixed(2)}€
+                </span>
+              </strong>
+            </div>
+            <div className='flex justify-between my-4'>
+              <Button
+                href='/checkout'
+                onClick={onClose}
+                variant='primary'
+                position='card'
+              >
+                <CartIcon height={20} width={20} />
+                To Checkout
+              </Button>
+              <Button
+                as='button'
+                type='button'
+                onClick={clearCart}
+                variant='danger'
+                position='card'
+              >
+                <TrashIcon height={20} width={20} />
+                Clear Cart
+              </Button>
+            </div>
+          </div>
+          <ul className='list-none m-0 p-0 space-y-3'>
+            {cartItems.map((item) => (
+              <li key={item.id}>
+                <Link
+                  href={`/product/${item.slug}`}
+                  className='p-2 border border-[#1e9c29] rounded-lg flex gap-2 justify-between bg-[#103d22] hover:bg-[#134929] transition-colors'
+                  onClick={onClose}
+                >
+                  <div className='flex gap-2'>
+                    <div className='bg-white min-w-24 h-24 w-24 block rounded-lg overflow-hidden relative'>
+                      {item.isOnSale && (
+                        <div className='absolute text-sm bg-red-700 h-8 grid place-items-center w-32 top-[8px] right-[-40px] rotate-45 z-50 will-change-transform'>
+                          On Sale
+                        </div>
+                      )}
+                      <Image
+                        className='object-contain w-full'
+                        src={`https://utfs.io/a/5sfnefg5kv/${item.imageUrl}`}
+                        height={90}
+                        width={90}
+                        alt={item.name}
+                        draggable='false'
+                        decoding='async'
+                        loading='lazy'
+                      />
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                      <strong className='line-clamp-1 text-lg'>
+                        {item.name}
+                      </strong>
+                      <div className='flex items-center gap-2'>
+                        <span className='text-sm'>In Your Cart:</span>
+                        <span>{item.quantity}</span>
+                      </div>
+                      <div className='flex items-center gap-2'>
+                        {item.isOnSale && item.reducedPrice ? (
+                          <>
+                            <span className='text-sm'>Price:</span>
+                            <span className='text-red-500 font-bold'>
+                              {item.reducedPrice.toFixed(2)}€
+                            </span>
+                            <span className='text-xs text-red-500 line-through'>
+                              {item.price.toFixed(2)}€
+                            </span>
+                          </>
+                        ) : (
+                          <>
+                            <span className='text-sm'>Price:</span>
+                            <span>{item.price.toFixed(2)}€</span>
+                          </>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                  <div className='min-w-4'>
+                    <Button
+                      as='button'
+                      variant='danger'
+                      position='standalone'
+                      onClick={(e) => handleRemoveFromCart(e, item.id)}
+                      className='rounded-sm'
+                    >
+                      <CloseIcon height={20} width={20} />
+                    </Button>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p className={className}>Your shopping cart is empty.</p>
+      )}
+    </>
+  );
+}
