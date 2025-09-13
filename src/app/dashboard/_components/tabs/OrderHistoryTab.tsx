1c3c7f7ea590dadafa84f5c8c5ffcae0c2dd0a0a
OneDev49 <dev49.sgra@protonmail.com>
2025-09-14 01:09:23 +0200

    fix(dashboard): fix index map error for OrderHistory

--- src/app/dashboard/_components/tabs/OrderHistoryTab.tsx
+++ src/app/dashboard/_components/tabs/OrderHistoryTab.tsx
@@ -13,13 +13,15 @@ import Link from 'next/link';
 
 type filterOption = 'all' | 'delivered' | 'processing' | 'canceled';
 
+interface DashboardOrderHistoryProps {
+  user: TabUser;
+  orders: TabOrder[];
+}
+
 export default function DashboardOrderHistory({
   user,
   orders,
-}: {
-  user: TabUser;
-  orders: TabOrder[];
-}) {
+}: DashboardOrderHistoryProps) {
   const [filter, setFilter] = useState<filterOption>('all');
   const [expandedId, setExpandedId] = useState<string | null>(null);
 
@@ -64,160 +66,155 @@ export default function DashboardOrderHistory({
                   <th className='p-2'></th>
                 </tr>
               </thead>
-              <tbody>
-                {filteredOrders.length > 0 &&
-                  filteredOrders.map((order, index) => {
-                    const totalProductCost: number = order.products.reduce(
-                      (total, product) =>
-                        total + product.price * product.quantity,
-                      0
-                    );
+              {filteredOrders.length > 0 &&
+                filteredOrders.map((order, index) => {
+                  const totalProductCost: number = order.products.reduce(
+                    (total, product) =>
+                      total + product.price * product.quantity,
+                    0
+                  );
 
-                    const totalProductAmount: number = order.products.reduce(
-                      (total, product) => total + product.quantity,
-                      0
-                    );
-                    return (
-                      <>
-                        <tr
-                          key={index}
-                          className={clsx(
-                            'hover:bg-green-800 cursor-pointer select-none',
-                            expandedId === order.id && 'bg-green-900'
-                          )}
-                          onClick={() =>
-                            setExpandedId(
-                              expandedId === order.id ? null : order.id
-                            )
-                          }
-                        >
-                          <td className='p-2'>{order.id}</td>
-                          <td className='p-2'>{totalProductAmount}</td>
-                          <td className='p-2'>{order.deliveryDate}</td>
-                          <td className='p-2'>
-                            {totalProductCost.toFixed(2)}€
-                          </td>
-                          <td className='p-2'>
+                  const totalProductAmount: number = order.products.reduce(
+                    (total, product) => total + product.quantity,
+                    0
+                  );
+                  return (
+                    <tbody key={index}>
+                      <tr
+                        className={clsx(
+                          'hover:bg-green-800 cursor-pointer select-none',
+                          expandedId === order.id && 'bg-green-900'
+                        )}
+                        onClick={() =>
+                          setExpandedId(
+                            expandedId === order.id ? null : order.id
+                          )
+                        }
+                      >
+                        <td className='p-2'>{order.id}</td>
+                        <td className='p-2'>{totalProductAmount}</td>
+                        <td className='p-2'>{order.deliveryDate}</td>
+                        <td className='p-2'>{totalProductCost.toFixed(2)}€</td>
+                        <td className='p-2'>
+                          <div
+                            className={clsx(
+                              'rounded-3xl text-center border-2',
+                              order.status === 'delivered' &&
+                                'border-green-400',
+                              order.status === 'processing' &&
+                                'border-yellow-200',
+                              order.status === 'canceled' && 'border-red-400'
+                            )}
+                          >
+                            {order.status.charAt(0).toUpperCase() +
+                              order.status.slice(1)}
+                          </div>
+                        </td>
+                        <td className='p-2'>
+                          <button
+                            type='button'
+                            title={`Expand Order Number ${order.id}`}
+                            aria-label={`Expand Order Number ${order.id}`}
+                            className='w-4 h-4 bg-white rounded-full grid place-items-center'
+                          >
+                            {expandedId === order.id ? (
+                              <ChevronUpIcon
+                                height={12}
+                                width={12}
+                                color='#000'
+                              />
+                            ) : (
+                              <ChevronDownIcon
+                                height={12}
+                                width={12}
+                                color='#000'
+                              />
+                            )}
+                          </button>
+                        </td>
+                      </tr>
+                      {expandedId === order.id && (
+                        <tr>
+                          <td colSpan={6}>
                             <div
                               className={clsx(
-                                'rounded-3xl text-center border-2',
-                                order.status === 'delivered' &&
-                                  'border-green-400',
-                                order.status === 'processing' &&
-                                  'border-yellow-200',
-                                order.status === 'canceled' && 'border-red-400'
+                                'space-y-2 p-2',
+                                expandedId === order.id &&
+                                  'bg-green-950 border-l border-r border-b border-green-800 space-y-2'
                               )}
                             >
-                              {order.status.charAt(0).toUpperCase() +
-                                order.status.slice(1)}
+                              <p>
+                                This is Order{' '}
+                                <span className='underline'>{order.id}</span>.
+                                The following Details are available for this
+                                Order:
+                              </p>
+                              <hr className='border-gray-600' />
+                              <div>
+                                <strong className='flex gap-1 items-center'>
+                                  <MapPinIcon height={20} width={20} />
+                                  Delivered to this Address:
+                                </strong>
+                                <table className='border-collapse ml-6'>
+                                  <tbody>
+                                    <tr>
+                                      <td>Street:</td>
+                                      <td>{order.address.street}</td>
+                                    </tr>
+                                    <tr>
+                                      <td>ZIP:</td>
+                                      <td>{order.address.zip}</td>
+                                    </tr>
+                                    <tr>
+                                      <td>City:</td>
+                                      <td>{order.address.city}</td>
+                                    </tr>
+                                    <tr>
+                                      <td className='pr-4'>Country:</td>
+                                      <td>{order.address.country}</td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </div>
+                              <hr className='border-gray-600' />
+                              <div>
+                                <strong className='flex gap-1 items-center'>
+                                  <CartIcon height={20} width={20} />
+                                  Following Products were ordered:
+                                </strong>
+                                <table className='border-collapse ml-6'>
+                                  <tbody>
+                                    <tr className='border-b-2 border-green-400'>
+                                      <td>Product</td>
+                                      <td className='px-5'>Qty</td>
+                                      <td>Price</td>
+                                    </tr>
+                                    {order.products.map((product, index) => (
+                                      <tr key={index}>
+                                        <td>
+                                          <Link
+                                            className='underline'
+                                            href={`/product/${product.id}`}
+                                          >
+                                            {product.name}
+                                          </Link>
+                                        </td>
+                                        <td className='text-center'>
+                                          {product.quantity}
+                                        </td>
+                                        <td>{product.price.toFixed(2)}€</td>
+                                      </tr>
+                                    ))}
+                                  </tbody>
+                                </table>
+                              </div>
                             </div>
                           </td>
-                          <td className='p-2'>
-                            <button
-                              type='button'
-                              title={`Expand Order Number ${order.id}`}
-                              aria-label={`Expand Order Number ${order.id}`}
-                              className='w-4 h-4 bg-white rounded-full grid place-items-center'
-                            >
-                              {expandedId === order.id ? (
-                                <ChevronUpIcon
-                                  height={12}
-                                  width={12}
-                                  color='#000'
-                                />
-                              ) : (
-                                <ChevronDownIcon
-                                  height={12}
-                                  width={12}
-                                  color='#000'
-                                />
-                              )}
-                            </button>
-                          </td>
                         </tr>
-                        {expandedId === order.id && (
-                          <tr>
-                            <td colSpan={6}>
-                              <div
-                                className={clsx(
-                                  'space-y-2 p-2',
-                                  expandedId === order.id &&
-                                    'bg-green-950 border-l border-r border-b border-green-800 space-y-2'
-                                )}
-                              >
-                                <p>
-                                  This is Order{' '}
-                                  <span className='underline'>{order.id}</span>.
-                                  The following Details are available for this
-                                  Order:
-                                </p>
-                                <hr className='border-gray-600' />
-                                <div>
-                                  <strong className='flex gap-1 items-center'>
-                                    <MapPinIcon height={20} width={20} />
-                                    Delivered to this Address:
-                                  </strong>
-                                  <table className='border-collapse ml-6'>
-                                    <tbody>
-                                      <tr>
-                                        <td>Street:</td>
-                                        <td>{order.address.street}</td>
-                                      </tr>
-                                      <tr>
-                                        <td>ZIP:</td>
-                                        <td>{order.address.zip}</td>
-                                      </tr>
-                                      <tr>
-                                        <td>City:</td>
-                                        <td>{order.address.city}</td>
-                                      </tr>
-                                      <tr>
-                                        <td className='pr-4'>Country:</td>
-                                        <td>{order.address.country}</td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                </div>
-                                <hr className='border-gray-600' />
-                                <div>
-                                  <strong className='flex gap-1 items-center'>
-                                    <CartIcon height={20} width={20} />
-                                    Following Products were ordered:
-                                  </strong>
-                                  <table className='border-collapse ml-6'>
-                                    <tbody>
-                                      <tr className='border-b-2 border-green-400'>
-                                        <td>Product</td>
-                                        <td className='px-5'>Qty</td>
-                                        <td>Price</td>
-                                      </tr>
-                                      {order.products.map((product, index) => (
-                                        <tr key={index}>
-                                          <td>
-                                            <Link
-                                              className='underline'
-                                              href={`/product/${product.id}`}
-                                            >
-                                              {product.name}
-                                            </Link>
-                                          </td>
-                                          <td className='text-center'>
-                                            {product.quantity}
-                                          </td>
-                                          <td>{product.price.toFixed(2)}€</td>
-                                        </tr>
-                                      ))}
-                                    </tbody>
-                                  </table>
-                                </div>
-                              </div>
-                            </td>
-                          </tr>
-                        )}
-                      </>
-                    );
-                  })}
-              </tbody>
+                      )}
+                    </tbody>
+                  );
+                })}
             </table>
           ) : (
             <div className='px-3'>
