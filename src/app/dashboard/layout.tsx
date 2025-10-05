f9ca4afb9b875290610b8d59d3ed3694b75e2279
OneDev49 <dev49.sgra@protonmail.com>
2025-10-05 17:46:45 +0200

    feat: add metadata to user dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,37 @@ import HeaderLayout from '@/components/layout/Header';
 import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import { authOptions } from '../api/auth/[...nextauth]/route';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'User Dashboard',
+  description:
+    'Your personal Entro User Dashboard for your Account. Manage all your settings, your orders, your Cart and your Wishlist comfortable from your own Dashboard.',
+
+  openGraph: {
+    title: 'User Dashboard',
+    description:
+      'Your personal Entro User Dashboard for your Account. Manage all your settings, your orders, your Cart and your Wishlist comfortable from your own Dashboard.',
+  },
+
+  twitter: {
+    title: 'User Dashboard',
+    description:
+      'Your personal Entro User Dashboard for your Account. Manage all your settings, your orders, your Cart and your Wishlist comfortable from your own Dashboard.',
+  },
+
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+};
 
 export default async function RootLayout({
   children,
