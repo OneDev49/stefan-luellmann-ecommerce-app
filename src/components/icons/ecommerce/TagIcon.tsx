b779a12cd5237bef7c7807f58916b22a372cb9bd
OneDev49 <dev49.sgra@protonmail.com>
2025-10-05 17:21:35 +0200

    feat(icon): create TagIcon ui component

---
+++ src/components/icons/ecommerce/TagIcon.tsx
@@ -0,0 +1,49 @@
+/**
+ * @license
+ * SVG Code by:
+ * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license/free
+ * Copyright 2025 Fonticons, Inc.
+ *
+ * For more information, check the LICENSE.txt of the Repository
+ */
+
+const iconVariants = {
+  solid: {
+    viewBox: '0 0 448 512',
+    path: 'M0 80L0 229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7L48 32C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z',
+  },
+};
+
+type IconVariant = keyof typeof iconVariants;
+
+interface IconProps {
+  width?: number | string;
+  height?: number | string;
+  className?: string;
+  color?: string;
+  variant?: IconVariant;
+}
+
+export default function TagIcon({
+  width = 24,
+  height = 24,
+  className,
+  variant = 'solid',
+  color,
+}: IconProps) {
+  const selectedVariant = iconVariants[variant];
+
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox={selectedVariant.viewBox}
+      width={width}
+      height={height}
+      fill={color || 'currentColor'}
+      className={className}
+    >
+      <path d={selectedVariant.path} />
+    </svg>
+  );
+}
