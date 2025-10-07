722cf540a2285a08acb5a26d9286ca21fc6ec881
OneDev49 <dev49.sgra@protonmail.com>
2025-10-07 17:30:55 +0200

    feat: add metadata to login and register pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,6 +2,25 @@ import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import LoginForm from './_components/LoginForm';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description:
+    'Log into your Entro Account to manage all your Orders, safe your Shopping Cart, safe your Wishlist, manage your account and so much more.',
+
+  openGraph: {
+    title: 'Login',
+    description:
+      'Log into your Entro Account to manage all your Orders, safe your Shopping Cart, safe your Wishlist, manage your account and so much more.',
+  },
+
+  twitter: {
+    title: 'Login',
+    description:
+      'Log into your Entro Account to manage all your Orders, safe your Shopping Cart, safe your Wishlist, manage your account and so much more.',
+  },
+};
 
 export default async function LoginPage() {
   const session = await getServerSession(authOptions);
