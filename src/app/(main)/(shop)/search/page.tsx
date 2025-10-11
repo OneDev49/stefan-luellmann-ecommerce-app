fbb45ad0ee00d0734f226fdd22e1f78ca070669e
OneDev49 <dev49.sgra@protonmail.com>
2025-10-11 23:51:03 +0200

    refactor(prisma): update prisma queries on search and product page

--- src/app/(main)/(shop)/search/page.tsx
+++ src/app/(main)/(shop)/search/page.tsx
@@ -1,10 +1,12 @@
-import { cache } from 'react';
-import { prisma } from '@/lib/prisma';
-import { Prisma } from '@prisma/client';
 import { mapToProductCard } from '@/lib/mappers/product';
+import { Metadata } from 'next';
+import {
+  getAllBrands,
+  getAllCategories,
+  getAllProductsForSearch,
+} from '@/lib/data/products';
 
 import SearchPageClient from './_components/SearchPageClient';
-import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Search',
@@ -24,19 +26,9 @@ export const metadata: Metadata = {
   },
 };
 
-// Cache categories for better optimization on Project since Products never change
-const getAllCategories = cache(async () => {
-  return prisma.category.findMany({
-    select: { name: true, slug: true },
-    orderBy: { name: 'asc' },
-  });
-});
-
 type SearchParams = {
   q?: string;
 };
-
-const MAXIMUM_AMOUNT_OF_DEMO_PRODUCTS = 200;
 
 export default async function SearchPage({
   searchParams,
@@ -45,44 +37,19 @@ export default async function SearchPage({
 }) {
   const searchTerm = searchParams.q;
 
-  // Minimal where clause only for search term
-  const where: Prisma.ProductWhereInput = {};
-
-  if (searchTerm) {
-    where.OR = [
-      { name: { contains: searchTerm, mode: 'insensitive' } },
-      { brand: { contains: searchTerm, mode: 'insensitive' } },
-      { shortDescription: { contains: searchTerm, mode: 'insensitive' } },
-    ];
-  }
-
   // Single DB Query to fetch all matching products at once
-  const [allProductsDB, allCategories] = await Promise.all([
-    prisma.product.findMany({
-      where,
-      take: MAXIMUM_AMOUNT_OF_DEMO_PRODUCTS,
-      include: {
-        categories: {
-          select: {
-            slug: true,
-            name: true,
-          },
-        },
-      },
-    }),
+  const [allProductsDB, allCategories, allBrands] = await Promise.all([
+    getAllProductsForSearch(searchTerm),
     getAllCategories(),
+    getAllBrands(),
   ]);
 
   const allProducts = allProductsDB.map(mapToProductCard);
 
-  const uniqueBrands = Array.from(
-    new Set(allProductsDB.map((p) => p.brand))
-  ).sort();
-
   return (
     <SearchPageClient
       allProducts={allProducts}
-      brands={uniqueBrands}
+      brands={allBrands}
       categories={allCategories}
       searchTerm={searchTerm}
     />
