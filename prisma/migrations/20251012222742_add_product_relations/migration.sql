8b1bff6a41646ccac2fe13d456402cc882e287e5
OneDev49 <dev49.sgra@protonmail.com>
2025-10-14 23:56:06 +0200

    feat(db): update prisma model for cart/wishlist DB implementation

---
+++ prisma/migrations/20251012222742_add_product_relations/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "updatedAt" DROP DEFAULT;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WishlistItem" ADD CONSTRAINT "WishlistItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
