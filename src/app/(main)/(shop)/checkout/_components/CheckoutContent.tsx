717a0eb1d6d031616a80f5c776c00e73c5bd2833
OneDev49 <dev49.sgra@protonmail.com>
2025-10-19 22:52:10 +0200

    refactor: optimize checkout page and add new store hooks

--- src/app/(main)/(shop)/checkout/_components/CheckoutContent.tsx
+++ src/app/(main)/(shop)/checkout/_components/CheckoutContent.tsx
@@ -1,19 +1,20 @@
 'use client';
 
+import { useCart } from '@/hooks/useCart';
+import { selectTotalItems, useCartStore } from '@/store/cartStore';
+
 import AnglesRightIcon from '@/components/icons/ui/AnglesRightIcon';
 import Button from '@/components/ui/Button';
-import { selectTotalItems, useCartStore } from '@/store/cartStore';
 import clsx from 'clsx';
 import CheckoutCard from './CheckoutCard';
 import CheckoutTotal from './CheckoutTotal';
 
 export default function CheckoutContent() {
-  const cartItems = useCartStore((state) => state.items);
+  const { items } = useCart();
   const totalCartAmount = useCartStore(selectTotalItems);
 
-  const transparentCardClassName = clsx(
-    'bg-[rgb(33,33,33,0.5)] border border-[#6c6c6c] rounded-xl flex flex-col overflow-hidden'
-  );
+  const transparentCardClassName =
+    'bg-[rgb(33,33,33,0.5)] border border-[#6c6c6c] rounded-xl flex flex-col overflow-hidden';
   const checkoutContentClassName = clsx({
     ['w-full md:basis-9/12']: totalCartAmount > 0,
     ['grow']: totalCartAmount === 0,
@@ -26,15 +27,15 @@ export default function CheckoutContent() {
         className={`${checkoutContentClassName} ${transparentCardClassName}`}
       >
         <h2 className='text-3xl font-bold p-4 bg-[rgb(87,87,87,0.2)] shadow-[0_4px_15px_0_rgb(0,0,0,1)]'>
-          {cartItems.length > 1
+          {items.length > 1
             ? `${totalCartAmount} Products`
             : `${totalCartAmount} Product`}{' '}
           in your Cart
         </h2>
         <div className='px-2 lg:px-8'>
-          {cartItems.length > 0 ? (
+          {items.length > 0 ? (
             <ul className='list-none m-0'>
-              {cartItems.map((item) => (
+              {items.map((item) => (
                 <li
                   key={item.id}
                   className='border-b border-[#6c6c6c] last:border-0 py-3 lg:py-6'
@@ -58,7 +59,7 @@ export default function CheckoutContent() {
           )}
         </div>
       </div>
-      {cartItems.length > 0 && (
+      {items.length > 0 && (
         <div
           className={`w-full md:basis-[300px] lg:basis-[320px] md:sticky md:top-28 space-y-6 ${transparentCardClassName}`}
         >
