5e64b48060d87720a09e5b1a23f7d1d1b69c87bb
OneDev49 <dev49.sgra@protonmail.com>
2025-10-31 23:14:14 +0100

    feat: add discount and tax to checkout. upgrade style of checkout

--- src/app/(main)/(shop)/checkout/_components/CheckoutTotal.tsx
+++ src/app/(main)/(shop)/checkout/_components/CheckoutTotal.tsx
@@ -13,35 +13,89 @@ export default function CheckoutTotal() {
   const taxAmount = totalCartValue * 0.19;
   const taxedTotal = totalCartValue + taxAmount;
 
+  const listItemClassName =
+    'flex justify-between gap-2 border-b border-gray-600 border-dashed last:border-0 py-2';
+
+  // Summary Calculations
+  const TAX_AMOUNT: number = 0.19;
+  const DISCOUNT_AMOUNT: number = 0.1;
+  const SHIPPING_COSTS: number = 7.99;
+  const SHIPPING_METHOD: string = 'Standard';
+
+  const totalTaxAmount: number =
+    (totalCartValue - totalCartValue * DISCOUNT_AMOUNT + SHIPPING_COSTS) *
+    TAX_AMOUNT;
+
+  const totalAmount: number =
+    totalCartValue -
+    totalCartValue * DISCOUNT_AMOUNT +
+    SHIPPING_COSTS +
+    totalTaxAmount;
   return (
     <>
       <h2 className='text-3xl font-bold p-4 bg-[rgb(87,87,87,0.2)] shadow-[0_4px_15px_0_rgb(0,0,0,1)]'>
         Checkout
       </h2>
       <div className='px-3 space-y-2'>
-        <div className='flex flex-col gap-2 border-b border-white pb-4'>
-          <strong className='flex justify-between'>
-            <span>Current Subtotal:</span>
-            <span className='text-[#2aff00]'>{totalCartValue.toFixed(2)}€</span>
-          </strong>
-          <strong className='flex justify-between'>
-            <span>Tax(19%):</span>
-            <span className='text-[#2aff00]'>{taxAmount.toFixed(2)}€</span>
-          </strong>
-        </div>
-        <h3 className='text-xl flex justify-between font-bold'>
+        <ul className='list-none flex flex-col pb-4'>
+          <li className={listItemClassName}>
+            <span className='font-bold'>Subtotal</span>
+            <strong>{totalCartValue.toFixed(2)}€</strong>
+          </li>
+          {DISCOUNT_AMOUNT > 0 && (
+            <li className={listItemClassName}>
+              <div className='space-x-2'>
+                <span>Discount</span>
+                <span className='text-sm text-brightGreen'>
+                  ({DISCOUNT_AMOUNT * 100}%)
+                </span>
+              </div>
+              <span>-{(totalCartValue * DISCOUNT_AMOUNT).toFixed(2)}€</span>
+            </li>
+          )}
+          <li className={listItemClassName}>
+            <div className='space-x-2'>
+              <span>Shipping</span>
+              <span className='text-sm text-brightGreen'>
+                ({SHIPPING_METHOD})
+              </span>
+            </div>
+            <span>{SHIPPING_COSTS.toFixed(2)}€</span>
+          </li>
+          <li className={listItemClassName}>
+            <div className='space-x-2'>
+              <span>Tax</span>
+              <span className='text-sm text-brightGreen'>
+                ({TAX_AMOUNT * 100}%)
+              </span>
+            </div>
+            <span>{totalTaxAmount.toFixed(2)}€</span>
+          </li>
+        </ul>
+        <hr />
+        <h3 className='text-lg flex justify-between font-bold'>
           <span>Total Amount:</span>
-          <span className='text-[#2aff00]'>{taxedTotal.toFixed(2)}€</span>
+          <span className='text-brightGreen'>{taxedTotal.toFixed(2)}€</span>
         </h3>
       </div>
-      <div className='p-3 flex flex-col space-y-2 items-start'>
-        <Button href='/' variant='primary' position='card'>
+      <div className='p-3 flex justify-between gap-2 flex-row md:flex-col items-start'>
+        <Button
+          href='/'
+          variant='primary'
+          position='standalone'
+          className='py-2 px-4 rounded-md md:w-full md:justify-center'
+        >
           <>
             <CartIcon />
             <span>Proceed to Payment</span>
           </>
         </Button>
-        <Button href='/' variant='secondary' position='card'>
+        <Button
+          href='/search'
+          variant='secondary'
+          position='standalone'
+          className='py-2 px-4 rounded-md md:w-full md:justify-center'
+        >
           <>
             <span>Continue Shopping</span>
             <AnglesRightIcon />
