df2cea442f9165aa8b3d6f260011a35579c8f7c2
OneDev49 <dev49.sgra@protonmail.com>
2025-11-12 23:43:41 +0100

    fix(auth): separate authOptions from nextauth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,62 +1,6 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { prisma } from '@/lib/prisma';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import bcrypt from 'bcrypt';
+import { authOptions } from '@/lib/authOptions';
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    /* Email & Password Provider */
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'EMail', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) return null;
-
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
-        if (!user || !user.hashedPassword) return null;
-
-        const passwordsMatch = await bcrypt.compare(
-          credentials.password,
-          user.hashedPassword
-        );
-        if (!passwordsMatch) return null;
-
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-          image: user.image,
-        };
-      },
-    }),
-  ],
-  session: {
-    strategy: 'jwt',
-  },
-
-  callbacks: {
-    jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.id as string;
-      }
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import NextAuth from 'next-auth';
 
 const handler = NextAuth(authOptions);
 
